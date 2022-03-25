84aa08b4d6f1ece82bc6a2a05640fbee3b4159fe
sinyo2-ssu <mrazsin@gmail.com>
2022-03-25 16:30:19 +0900

    feat : cookie추가

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -2,6 +2,7 @@ package hello.servlet.basic.response;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,13 +19,28 @@ public class ResponseHeaderServlet extends HttpServlet {
         resp.setStatus(HttpServletResponse.SC_OK);
 
         //[response-line]
-        resp.setHeader("Content-Type", "text/plain");
+//        resp.setHeader("Content-Type", "text/plain;charset=utf-8");
         resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
         resp.setHeader("Pragma", "no-cache");
         resp.setHeader("my-header", "hello");
 
-        PrintWriter writer = resp.getWriter();
-        writer.println("ok");
 
+        //[Header 편의 메서드]
+        content(resp);
+        cookie(resp);
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("OK");
+    }
+
+    private void content(HttpServletResponse response){
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+    }
+
+    private void cookie(HttpServletResponse response){
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600);
+        response.addCookie(cookie);
     }
 }
