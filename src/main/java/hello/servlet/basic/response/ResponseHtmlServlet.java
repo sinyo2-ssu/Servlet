5c7669744f4d30c002f222121dd611d30811e2c3
sinyo2-ssu <mrazsin@gmail.com>
2022-03-29 09:07:01 +0900

    220329

--- src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -1,4 +1,17 @@
 package hello.servlet.basic.response;
 
-public class ResponseHtmlServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
