89c6f58f48509a01915872914e13421397ad6a75
sinyo2-ssu <mrazsin@gmail.com>
2022-04-11 16:27:03 +0900

    220411

--- src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -17,8 +17,9 @@ import java.util.Map;
 public class FrontControllerServletV1 extends HttpServlet {
 
     private Map<String, ControllerV1> controllerMap = new HashMap<>();
-    
+
     public FrontControllerServletV1() {
+
         controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
         controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
         controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
