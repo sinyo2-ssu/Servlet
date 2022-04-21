d8e836e514c9e4bb5d0a1def5d0e1e813fe09047
sinyo2-ssu <mrazsin@gmail.com>
2022-04-21 14:33:40 +0900

    220421

--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -26,8 +26,6 @@ public class MemberSaveControllerV2 implements ControllerV2 {
 
         //Model에 데이터를 보관한다.
         request.setAttribute("member", member);
-
         return new MyView("/WEB-INF/views/save-result.jsp");
     }
-
 }
